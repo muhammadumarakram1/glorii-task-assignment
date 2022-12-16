559534453229f8461868e2ab4a432cbe2f3a9aee
muhammadumarakram1 <muhammadumarakram1@gmail.com>
2022-12-16 17:49:42 +0500

    Error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -15,31 +15,31 @@ const sendError = (err, res) => {
 };
 
 const handleCastErrorDB = (err) => {
+	console.log(err);
 	const message = `Invalid ${err.path}: ${err.value}`;
 	return new AppError(message, 400);
 };
 
 const handleDuplicateFieldsDB = (err) => {
-	console.log(err.keyValue);
-	const value = Object.keys(err.keyValue);
-	const message = `Duplicate field value: ${value}. Please use another value!`;
+	const key = Object.keys(err.keyValue);
+	const value = Object.values(err.keyValue);
+	const message = `Duplicate field (${key} :${value}). Please use another value!`;
 	return new AppError(message, 400);
 };
 
 const handleValidationErrorDB = (err) => {
-	console.log(err);
 	const errors = Object.values(err.errors).map((el) => el.message);
 	const message = `Invalid input data. ${errors.join('. ')}`;
 	return new AppError(message, 400);
 };
 
 module.exports = (err, req, res, next) => {
-	console.log(err);
 	let error = { ...err };
 	error.statusCode = error.statusCode || 500;
 	error.status = error.status || 'error';
-	error.message = error._message || 'Something went wrong';
-	if (error.name === 'CastError') error = handleCastErrorDB(error);
+	error.message = err.message || error._message || 'Something went wrong';
+	console.log(error.path);
+	if (error.path === '_id') error = handleCastErrorDB(error);
 	if (error.code === 11000) error = handleDuplicateFieldsDB(error);
 	if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
 	sendError(error, res);
